969fb8e987c18ee5a1a0f2732bee8445d0d7fe02
madflow3 <alexandre@gmail.com>
2014-01-11 17:13:51 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LandpApp::Application.config.secret_key_base = 'e4523cd59a5a2a88c8091b684bcca1f496987de6226f7d763dc592f5ece5a0f15ac971087e599bae59d36e9bd247d8d985f932bb5429a2047eec5648b8106d17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LandpApp::Application.config.session_store :cookie_store, key: '_landp_app_session'
