21defd28160eed5acf1d8e4b8a4f3e13d89aec52
madflow3 <alexandre@gmail.com>
2014-03-05 21:58:34 +0100

    maj5214

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -7,9 +7,20 @@ end
 def create
 	@association = Association.new association_params
 	@association.save if @association.valid?
-	gb = ("55c2899ec9189665ff2b7cce4692dd69-us3")	
-	list_id = b2845c8399
+	gb = Gibbon::API.new("55c2899ec9189665ff2b7cce4692dd69-us3")	
+	list_id = "b2845c8399"
 
+begin
+	gb.lists.subscribe({
+	:id => list_id,
+	:email => {:email => params[:email]},
+	:double_optin => true
+	})
+	notice = "C'est pratiquement fini;) Merci de confirmer votre inscription"
+rescue Gibbon::MailChimpError => e 
+	notice = e.message
+end
+	#redirect_to root_url, notice: notice
 end
 
 private
